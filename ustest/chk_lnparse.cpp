c0710a480578f8c11303409766a38a33dfcbcabc
dosconio <dosconyo@gmail.com>
2025-04-13 23:10:21 +0800

    202504A

--- ustest/chk_lnparse.cpp
+++ ustest/chk_lnparse.cpp
@@ -5,7 +5,15 @@
 
 using namespace uni;
 
-char glbbuf[] = "AB C +\n\n  123 \nXYZ.0";
+char glbbuf[] =
+
+"123456789012345678901234567890\n"
+"%include 123\n"
+"AB \"C\\\'\\x20C\\0A\" + 6\n"
+"%include -123\n"
+"#comment-line\n"
+"-123.5e+5-0.5 0.x # this is comment\n"
+"  123 \n_XYZ_.0\n\n";
 char* glbbpt = glbbuf;
 
 class MyInn : public IstreamTrait {
@@ -22,13 +30,17 @@ int main() {
 		LinearParser lnparse(myInn);
 		Dchain dc;
 
-		lnparse.Parse(dc);
+		lnparse.method_directive = '%';
+		lnparse.method_string_single_quote = true;
+		lnparse.method_string_double_quote = true;
+		// lnparse.method_treatiden_underscore = false;
+		// lnparse.method_omit_spaces = false;
 
 		Console.OutFormat("%s\n", glbbuf);
 
-		Tnode* tn = (Tnode*)dc.Root();
+		Tnode* tn = (Tnode*)lnparse.Parse(dc).Root();
 		while (tn = tn->next) {
-			Console.OutFormat(">%d(%u %u) \'%s\'\n", tn->type, tn->row, tn->col, tn->addr);
+			Console.OutFormat(">%d(%u %u) |%s|\n", tn->type, tn->row, tn->col, tn->addr);
 		}
 
 
