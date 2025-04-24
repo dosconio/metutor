926a638b3e0542a2098abe2acfa570a446aeb26d
dosconio <dosconyo@gmail.com>
2025-04-24 16:55:55 +0800

    202504B

---
+++ ustest/algorithm/chk_sort.cpp
@@ -0,0 +1,33 @@
+﻿#include <cpp/unisym>
+#include <cpp/nodes>
+#include <c/consio.h>
+
+using namespace uni;
+
+int tocomp_ft(pureptr_t a, pureptr_t b) {
+	Letvar(aa, Dnode*, a);
+	Letvar(bb, Dnode*, b);
+	return _IMM(aa->offs) - _IMM(bb->offs);
+}
+
+int main() {
+	Dchain a;
+	a.Append((rostr)4);
+	a.Append((rostr)7);
+	a.Append((rostr)1);
+	a.Append((rostr)9);
+	a.Append((rostr)6);
+	a.Append((rostr)0);
+	a.Compare_f = tocomp_ft;
+	//a.Sort(SortMode::Insertion);// AKA Sort(a, SortMode::Insertion) or  a.Sort();
+	//a.Sort(SortMode::Selection);
+	//a.Sort(SortMode::Bubble);
+	//a.Sort(SortMode::Heap);
+	//a.Sort(SortMode::Merge);
+	a.Sort(SortMode::Quick);
+	for0(i, a.Count()) {
+		Console.OutFormat("%d: %[u]\n", i, (stduint)a[i]->addr);
+	}
+	return 0;
+}
+
--- ustest/chk_lnparse.cpp
+++ ustest/chk_lnparse.cpp
@@ -5,15 +5,39 @@
 
 using namespace uni;
 
-char glbbuf[] =
+const char* tab_tokentype[] =
+{
+	"NON", "ANY",
+	"SYM", "SPA", "NUM", "IDN", "XXX",
+	"COM", "DIR", "STR",  "EOF", "FUN",
+	"EOF","complex","position","wides",
+	"u8str", // ...
+};
 
+char glbbuf[] =
+#if 0 && 0
 "123456789012345678901234567890\n"
 "%include 123\n"
+"  %include 123\n"
 "AB \"C\\\'\\x20C\\0A\" + 6\n"
 "%include -123\n"
 "#comment-line\n"
-"-123.5e+5-0.5 0.x # this is comment\n"
+"-123.5e+5-0.5 % 0.x # this is comment\n"
 "  123 \n_XYZ_.0\n\n";
+#endif
+
+"/* test *//**//*1*/" "\n"
+"#include \\" "\n"
+"\t<stdio.h> // at other line, but still a directive (directive but comment)" "\n"
+"int i\\\nj = 1\\" "\n"
+"\\" "\n"
+"2;// 12， consider this token at the line 'int i = ...'" "\n"
+"1=\\" "\n"
+"=2; 2\\3;" "\n"
+"// abc\\" "\n"
+"still the comment. And is a token with \\n" "\n";
+
+
 char* glbbpt = glbbuf;
 
 class MyInn : public IstreamTrait {
@@ -30,17 +54,23 @@ int main() {
 		LinearParser lnparse(myInn);
 		Dchain dc;
 
+#if 0 && 0
 		lnparse.method_directive = '%';
 		lnparse.method_string_single_quote = true;
 		lnparse.method_string_double_quote = true;
 		// lnparse.method_treatiden_underscore = false;
 		// lnparse.method_omit_spaces = false;
+#endif
+		lnparse.method_directive = '#';
+		lnparse.method_string_double_quote = true;
+		lnparse.handler_comment = LineParse_Comment_C_Cpp;
 
 		Console.OutFormat("%s\n", glbbuf);
 
 		Tnode* tn = (Tnode*)lnparse.Parse(dc).Root();
 		while (tn = tn->next) {
-			Console.OutFormat(">%d(%u %u) |%s|\n", tn->type, tn->row, tn->col, tn->addr);
+			Console.OutFormat(">%s(%u %u) |%s|\n", tab_tokentype[tn->type],
+				tn->row, tn->col, tn->addr);
 		}
 
 
